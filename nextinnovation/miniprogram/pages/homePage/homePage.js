91c9edfa59ceea159d253a9e689ab1830871425c
arashi5 <908189982@qq.com>
2019-11-11 17:23:22 +0800

    homePage

---
+++ nextinnovation/miniprogram/pages/homePage/homePage.js
@@ -0,0 +1,87 @@
+// miniprogram/pages/homePage/homePage.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    memoList: [{
+      title: 'FPX wins the game',
+      content: 'FPX 3:0 beat G2 Esports. Congratulations! FPX 3:0 beat G2 Esports. Congratulations! FPX 3:0 beat G2 Esports. Congratulations!'
+    }, {
+      title: 'FPX wins the game',
+      content: 'GG to G2 as well'
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  },
+
+  onLongtap(e){
+    wx.showModal({
+      title: 'Wanna Delete?',
+      content: 'Let\'s finish the job',
+      success(res) {
+        if (res.confirm) {
+          console.log(e)
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
+  }
+
+})
